f0aa0adb823cbced4ce47891160545678c866301
j0 <949561391@qq.com>
2016-09-29 10:04:08 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var log = require('log4js').getLogger('log_boot')
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var conf = require('./conf')
+
+//初始化日志配置
+require('./init/log')
+
+var app = express();
+
+// 设置视图引擎
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//网站图标
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+//加载compass
+if (conf.boot.env == 'development') {
+    log.debug('init compass')
+    app.use(require('node-compass')({mode: 'expanded'}));
+} else {
+    log.debug('do not init compass')
+}
+
+//静态资源
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+//路由管理
+app.use('/', routes);
+app.use('/users', users);
+
+// 404错误页面
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// 异常处理器
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error/error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
---
+++ conf/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by j0 on 2016/9/27.
+ */
+var conf = {
+    boot: {
+        port: 80,
+        env: 'development'
+    }
+}
+module.exports = conf
